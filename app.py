77b8096b46041e2175fe45ebfeb0a94744992e05
Sol <87423227+SolarGravel@users.noreply.github.com>
2025-08-25 09:35:18 -0300

    Add Projects page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ def main() -> None:
 @app.route("/")
 def index():
     return render_template("index.jinja")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.jinja")
 
 
 if __name__ == '__main__':
