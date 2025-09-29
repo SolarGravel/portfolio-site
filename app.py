76770bb0b9426c6d82234b73cc102dd08c46b2f6
Sol <87423227+SolarGravel@users.noreply.github.com>
2025-09-29 10:12:03 -0300

    Add modular JSON project list

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
+import json
 
 
 app = Flask(__name__)
+
+projects_list: list[dict] = []
 
 
 def main() -> None:
@@ -14,7 +17,14 @@ def index():
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.jinja")
+    projects_list = get_json("static/json/projects.json")
+    icons_list = get_json("static/json/icons.json")
+    
+    return render_template("projects.jinja", projects_list=projects_list, icons=icons_list)
+
+def get_json(json_file) -> list[dict]:
+    with open(json_file) as projects_json:
+        return json.load(projects_json)
 
 
 if __name__ == '__main__':
